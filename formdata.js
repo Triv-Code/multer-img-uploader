fe4ee39874cf97cdb27a45479222de830b34436d
Jacob Clark <77306976+Triv-Code@users.noreply.github.com>
2022-02-11 14:35:14 -0500

    Delete formdata.js

--- formdata.js
+++
@@ -1,9 +0,0 @@
-const FormData = require('form-data');
-const fs = require('fs');
-
-let form = new FormData();
-form.append( 'image', fs.createReadStream('/images/1644239056926--testimg.jpg'));
-
-
-// console.log(form);
-// module.exports.form = form;
